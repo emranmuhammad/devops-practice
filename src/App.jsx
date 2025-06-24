9423bc1a2264d2e05e62efe104289b630353b3a6
emranmuhammad <emran.a@xeleration.net>
2025-06-24 10:34:49 +0300

    Fix closing tag for div in App component

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      {/* </div> */}
       <NotFound/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
